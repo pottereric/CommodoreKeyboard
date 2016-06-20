ff4d291e7b4a06237e8f7369a64139c6b51ce8b9
Eric Potter <eric.b.potter@gmail.com>
2016-06-19 21:29:18 -0400

    all leters

--- CommodoreKeyboard.ino
+++ CommodoreKeyboard.ino
@@ -1,28 +1,6 @@
-/* @file HelloKeypad.pde
-|| @version 1.0
-|| @author Alexander Brevig
-|| @contact alexanderbrevig@gmail.com
-||
-|| @description
-|| | Demonstrates the simplest use of the matrix Keypad library.
-|| #
-*/
 #include <Keypad.h>
 
-//const byte ROWS = 8;
-//const byte COLS = 8; 
-//char keys[ROWS][COLS] = {
-//  {'2','4','6','8','0','-','A','B'},
-//  {'q','e','t','u','o','@','C','D'},
-//  {'z','z','z','z','z','z','z','z'},
-//  {'z','z','z','z','z','z','z','z'},
-//  {'z','z','z','z','z','z','z','z'},
-//  {'z','z','z','z','z','z','z','z'},
-//  {'G','w','r','y','i','p','*','H'},
-//  {'1','3','5','7','9','+','E','F'}
-//};
-//byte rowPins[ROWS] = {4,5,6,7,8,9,10,11}; 
-//byte colPins[COLS] = {19,18,17,16,15,14,13,12}; 
+
 
 const byte ROWS = 6;
 const byte COLS = 8; 
