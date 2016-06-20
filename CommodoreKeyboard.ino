c6b0437a288c75a29e91b3217d7f9d087f2da22c
Eric Potter <eric.b.potter@gmail.com>
2016-06-19 20:49:16 -0400

    works for number row

--- CommodoreKeyboard.ino
+++ CommodoreKeyboard.ino
@@ -9,20 +9,37 @@
 */
 #include <Keypad.h>
 
-const byte ROWS = 3;
-const byte COLS = 4; 
+//const byte ROWS = 8;
+//const byte COLS = 8; 
+//char keys[ROWS][COLS] = {
+//  {'2','4','6','8','0','-','A','B'},
+//  {'q','e','t','u','o','@','C','D'},
+//  {'z','z','z','z','z','z','z','z'},
+//  {'z','z','z','z','z','z','z','z'},
+//  {'z','z','z','z','z','z','z','z'},
+//  {'z','z','z','z','z','z','z','z'},
+//  {'G','w','r','y','i','p','*','H'},
+//  {'1','3','5','7','9','+','E','F'}
+//};
+//byte rowPins[ROWS] = {4,5,6,7,8,9,10,11}; 
+//byte colPins[COLS] = {19,18,17,16,15,14,13,12}; 
+
+const byte ROWS = 2;
+const byte COLS = 6; 
 char keys[ROWS][COLS] = {
-  {'1','4','7', '*'},
-  {'2','5','8', '0'},
-  {'3','6','9', '#'}
+  {'2','4','6','8','0','-'},
+  {'1','3','5','7','9','+'}
 };
-byte rowPins[ROWS] = {1, 2, 3}; 
-byte colPins[COLS] = {4, 5, 6, 7}; 
+byte rowPins[ROWS] = {4,11}; 
+byte colPins[COLS] = {19,18,17,16,15,14}; 
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
+  Serial.println("setup");
+  
+  
 }
   
 void loop(){
@@ -30,5 +47,6 @@ void loop(){
   
   if (key){
     Serial.println(key);
+    //if(key == 
   }
 }
